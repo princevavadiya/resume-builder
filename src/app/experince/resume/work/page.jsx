f314b75ab011d3177429c8ebf826a50b81666bc9
Prince <144765704+princevavadiya@users.noreply.github.com>
2025-04-17 16:01:19 +0530

    Update page.jsx

--- src/app/experince/resume/work/page.jsx
+++ src/app/experince/resume/work/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useRouter, useSearchParams } from "next/navigation";
+import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 
